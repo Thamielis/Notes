0076b7c29887e9cecc4d3c7e495382c842ac1224
Thamielis <52717133+Thamielis@users.noreply.github.com>
2025-06-26 16:45:53 +0200

    Extend repo guidelines and improve link coverage test

--- Tests/ValidateMarkdownLinks.Tests.ps1
+++ Tests/ValidateMarkdownLinks.Tests.ps1
@@ -22,8 +22,9 @@ Describe "Validate links in all index.md files" {
         $mdFiles = Get-ChildItem -Path $PathToTest -Filter index.md -Recurse -File
 
         foreach ($file in $mdFiles) {
-            #TODO: Context should show relative path starting from $RootPath (Replace $($file.Directory.Name))
-            Context "Folder: $($file.Directory.Name)" {
+            $relativeDir = [System.IO.Path]::GetRelativePath($RootPath, $file.Directory.FullName)
+            $relativeDir = $relativeDir -replace '\\', '/'
+            Context "Folder: $relativeDir" {
                 $content = Get-Content -Path $file.FullName -Raw
                 $links = Get-MarkdownLinks -Content $content | Where-Object { $_ -imatch '.md' }
                 
@@ -40,7 +41,37 @@ Describe "Validate links in all index.md files" {
             }
         }
 
-        #TODO: Another test should get the coverage of existing Markdown files linked in indexes.
+        Context "Coverage of markdown files referenced in indexes" {
+            It "outputs markdown coverage" {
+                $allLinks = foreach ($idx in $mdFiles) {
+                    $idxContent = Get-Content -Path $idx.FullName -Raw
+                    Get-MarkdownLinks -Content $idxContent | Where-Object { $_ -imatch '.md' } |
+                        ForEach-Object { $_.Replace('<', '').Replace('>', '') }
+                }
+
+                $allLinks = $allLinks | Select-Object -Unique
+
+                $allMdFiles = Get-ChildItem -Path $PathToTest -Filter *.md -Recurse -File | Where-Object { $_.Name -ne 'index.md' }
+
+                $notLinked = foreach ($md in $allMdFiles) {
+                    $relative = [System.IO.Path]::GetRelativePath($RootPath, $md.FullName) -replace '\\', '/'
+                    if ($allLinks -notcontains $relative) { $relative }
+                }
+
+                $coverage = if ($allMdFiles.Count -gt 0) {
+                    [math]::Round((($allMdFiles.Count - $notLinked.Count) / $allMdFiles.Count) * 100, 2)
+                } else { 100 }
+
+                Write-Host "Markdown coverage: $coverage% ($($allMdFiles.Count - $notLinked.Count)/$($allMdFiles.Count))"
+
+                if ($notLinked) {
+                    Write-Host "Unlinked files:" 
+                    $notLinked | ForEach-Object { Write-Host " - $_" }
+                }
+
+                $true | Should -BeTrue
+            }
+        }
     }
 
 }
